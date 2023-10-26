57bda77ae5d54ec53f01d825346c1abd2516987f
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2023-10-26 18:42:43 -0300

    Update ForDeHashMapChave-valor.java

--- Java/ForDeHashMapChave-valor.java
+++ Java/ForDeHashMapChave-valor.java
@@ -6,3 +6,17 @@ idadePorNome.put("Carol", 22);
 for (Map.Entry<String, Integer> entrada : idadePorNome.entrySet()) {
     System.out.println(entrada.getKey() + " tem " + entrada.getValue() + " anos.");
 }
+
+
+
+**************************
+
+ArrayList<String> listaDeNomes = new ArrayList<>();
+listaDeNomes.add("Alice");
+listaDeNomes.add("Bob");
+listaDeNomes.add("Carol");
+
+for (String nome : listaDeNomes) {
+    System.out.println(nome);
+}
+
