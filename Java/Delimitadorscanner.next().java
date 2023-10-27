45e6e4c7e0be2f6efed91e744ea55f66277e68f4
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2023-10-27 15:14:29 -0300

    Update Delimitadorscanner.next().java

--- Java/Delimitadorscanner.next().java
+++ Java/Delimitadorscanner.next().java
@@ -17,3 +17,27 @@ public class Main {
         scanner.close();
     }
 }
+
+
+************************
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner("Este é um exemplo de entrada.");
+
+        String palavra1 = scanner.next(); // Lê "Este"
+        String palavra2 = scanner.next(); // Lê "é"
+        String palavra3 = scanner.next(); // Lê "um"
+        String palavra4 = scanner.next(); // Lê "exemplo"
+        String palavra5 = scanner.next(); // Lê "de"
+
+        System.out.println("Palavra 1: " + palavra1);
+        System.out.println("Palavra 2: " + palavra2);
+        System.out.println("Palavra 3: " + palavra3);
+        System.out.println("Palavra 4: " + palavra4);
+        System.out.println("Palavra 5: " + palavra5);
+    }
+}
