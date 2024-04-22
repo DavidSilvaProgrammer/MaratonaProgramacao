ce4ff789b4693496ec2995af482816b26b6ab3c3
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-04-22 13:58:53 -0300

    Create ConcatenarPrintPython.py

---
+++ Python/1105-Sub-prime/ConcatenarPrintPython.py
@@ -0,0 +1,26 @@
+print("Olá, " + str(42) + " mundo!")
+
+
+*********************
+
+string1 = "Olá,"
+inteiro = 42
+string2 = "mundo!"
+
+concatenado = f"{string1} {inteiro} {string2}"
+print(concatenado)
+
+
+
+******************
+
+string1 = "Olá,"
+inteiro = 42
+string2 = "mundo!"
+
+concatenado = "{} {} {}".format(string1, inteiro, string2)
+print(concatenado)
+
+****************
+
+  
