ef0d75ae14742a2b452be4daddbab38e981c54ad
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-04-24 16:11:24 -0300

    Create ConcatenaçãoPrint.py

---
+++ Python/ConcatenaçãoPrint.py
@@ -0,0 +1,13 @@
+numero = 42
+print("O número é: " + str(numero))
+
+numero = 42
+print(f"O número é: {numero}")
+
+numero = 42
+outro_numero = 24
+print("Os números são: {} e {}".format(numero, outro_numero))
+
+numero = 42
+outro_numero = 24
+print("Os números são: {1} e {0}".format(numero, outro_numero))
