2587429fbb4697c9db7478832e65f0c0bbb7cd3c
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-04-27 10:42:21 -0300

    Update Strtok.c

--- LinguagemC/3424-AchandoOsMonótonosNão-Triviais Maximais/Strtok.c
+++ LinguagemC/3424-AchandoOsMonótonosNão-Triviais Maximais/Strtok.c
@@ -38,5 +38,20 @@ A função strtok utiliza um ponteiro por algumas razões:
 Em resumo, o uso de ponteiros na função strtok é essencial para manter o estado interno da função, 
 iterar através da string e modificar a string original conforme necessário para dividir a string em tokens.
 
+***************************
 
+
+
+A função strtok em C recebe dois parâmetros:
+
+    String a ser tokenizada: Este é um ponteiro para uma string que será dividida em tokens. 
+    A primeira vez que strtok é chamado, esta string é fornecida como o primeiro argumento. 
+    Nas chamadas subsequentes, este argumento deve ser NULL, indicando que a função deve 
+    continuar a tokenização da mesma string a partir de onde parou.
+
+    String de delimitadores: Este é um ponteiro para uma string contendo os caracteres que serão 
+    usados como delimitadores para dividir a string em tokens. Cada caractere na string de
+    delimitadores será considerado um delimitador. Quando strtok encontra um desses delimitadores 
+    na string a ser tokenizada, ele divide a string e retorna o token. Se vários delimitadores 
+    aparecerem consecutivamente, eles serão tratados como um único delimitador.
     */
