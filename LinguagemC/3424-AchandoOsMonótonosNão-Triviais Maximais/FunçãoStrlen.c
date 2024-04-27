7cb3979cc61ba17008ef7657bb7c0d31e4bf0980
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-04-27 10:30:03 -0300

    Create FunçãoStrlen.c

---
+++ LinguagemC/3424-AchandoOsMonótonosNão-Triviais Maximais/FunçãoStrlen.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[] = "Hello, world!";
+    int length = strlen(str);
+    printf("O comprimento da string é: %d\n", length);
+    return 0;
+}
