949164d291588cae0300c6222ccc884c656893ea
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-04-27 10:54:33 -0300

    Create 3424-ProfQuasePronta.c

---
+++ LinguagemC/3424-AchandoOsMonótonosNão-Triviais Maximais/3424-ProfQuasePronta.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[30];
+    char* token;
+    int tam,cont=0;
+    scanf("%s",str);
+    token = strtok(str,"b");
+    while(token!=NULL){
+        tam= strlen(token);
+        if(tam >1){
+            cont+=tam;
+        }
+        token=strtok(NULL, "b");
+    }
+    printf("%d\n",cont);
+    
+    return 0;
+}
