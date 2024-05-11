f4010b9f0b267c006174c713da55e8d0b2336ed7
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-05-11 07:26:28 -0300

    Create FuncaoModaModeImportStatistics.py

---
+++ Python/2658-AcordesIntergaláticos/FuncaoModaModeImportStatistics.py
@@ -0,0 +1,10 @@
+import statistics
+
+# Lista de números
+lista = [1, 2, 3, 3, 4, 4, 4, 5, 5]
+
+# Calcula a moda da lista
+moda = statistics.mode(lista)
+
+# Exibe a moda
+print("A moda da lista é:", moda)
