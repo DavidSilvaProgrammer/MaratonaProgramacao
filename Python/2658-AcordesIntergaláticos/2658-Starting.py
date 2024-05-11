f7178a2679d4f3afc4747b6dec4e3b2d434856dc
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-05-11 01:13:44 -0300

    Create 2658-Starting.py

---
+++ Python/2658-AcordesIntergaláticos/2658-Starting.py
@@ -0,0 +1,11 @@
+N, Q = map(int, input().split())
+
+lista_notas = [1] * N
+
+for i in range(Q):
+    A, B = map(int, input().split())
+        for j in range(A,B):
+            
+        
+        
+    
