6b71bfded6b03b9cfaf0a7f2475019fea5419075
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-05-11 06:54:32 -0300

    Update DicioContagemRepetição.py

--- Python/2658-AcordesIntergaláticos/DicioContagemRepetição.py
+++ Python/2658-AcordesIntergaláticos/DicioContagemRepetição.py
@@ -14,3 +14,23 @@ for caractere in lista:
 # Exibe a contagem de repetição de cada caractere
 for caractere, repeticao in contagem.items():
     print(f'O caractere "{caractere}" aparece {repeticao} vezes na lista.')
+ 
+################ Código semelhante ##################
+
+# Lista de números inteiros
+lista = [1, 2, 3, 4, 2, 1, 3, 4, 5, 5, 5]
+
+# Dicionário para armazenar a contagem de cada número
+contagem = {}
+
+# Percorre a lista e conta a repetição de cada número
+for numero in lista:
+    if numero in contagem:
+        contagem[numero] += 1
+    else:
+        contagem[numero] = 1
+
+# Exibe a contagem de repetição de cada número
+for numero, repeticao in contagem.items():
+    print(f'O número "{numero}" aparece {repeticao} vezes na lista.')
+
