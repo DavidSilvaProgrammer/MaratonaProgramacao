2a67a55894e831d45ff26519af933e0a2de45121
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-05-11 07:41:37 -0300

    Update 2658-Starting.py

--- Python/2658-AcordesIntergaláticos/2658-Starting.py
+++ Python/2658-AcordesIntergaláticos/2658-Starting.py
@@ -9,16 +9,20 @@ conta= {}
 for i in range(Q):
     A, B = map(int, input().split())
     lista_intervalo = lista_notas[A:B]
-    moda_intervalo = statistics.mode(lista_intervalo)
+    f = statistics.mode(lista_intervalo)
     
+    for i in range(A,B):
+        lista_notas[i]=(lista_notas[i]+f)%9
+        
+print(lista_notas)
+
+
     
-    
-    
-        for k in range(A,B):
-            if k in conta:
-                conta[k] +=1:
-            else:
-                conta[k]=1
+       # for k in range(A,B):
+        #    if k in conta:
+         #       conta[k] +=1:
+          #  else:
+           #     conta[k]=1
         
         
     
