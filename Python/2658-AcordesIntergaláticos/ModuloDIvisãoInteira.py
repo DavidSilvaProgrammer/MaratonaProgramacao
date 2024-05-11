b2a88ed0028ad459564d65a56ac7c2612704500b
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-05-11 00:28:30 -0300

    Create ModuloDIvisãoInteira.py

---
+++ Python/2658-AcordesIntergaláticos/ModuloDIvisãoInteira.py
@@ -0,0 +1,13 @@
+# Solicita dois números ao usuário
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+# Calcula o módulo
+modulo = numero1 % numero2
+
+# Calcula a divisão inteira
+divisao_inteira = numero1 // numero2
+
+# Exibe o resultado
+print("O módulo de", numero1, "dividido por", numero2, "é:", modulo)
+print("A divisão inteira de", numero1, "por", numero2, "é:", divisao_inteira)
