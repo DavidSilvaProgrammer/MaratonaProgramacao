c170bc148d5f6c92f3879f7c4e679897d7d7d520
David <109853684+DavidSilvaProgrammer@users.noreply.github.com>
2024-05-18 11:40:52 -0300

    Create 1926BaseSemSomar1.py

---
+++ Python/1926-Marianne e os Primos Gêmeos/1926BaseSemSomar1.py
@@ -0,0 +1,34 @@
+from math import sqrt
+
+def e_primo(numero):
+    if numero <= 1:
+        return False
+    for j in range(2, int(sqrt(numero)) + 1):
+        if numero % j == 0:
+            return False
+    return True
+
+def contar_primos_gemeos(lista_primos, k, p):
+    contador = 0
+    for i in range(len(lista_primos) - 1):
+        if lista_primos[i] >= k and lista_primos[i + 1] <= p and lista_primos[i + 1] - lista_primos[i] == 2:
+            contador += 1
+    # Adiciona 1 ao contador se k ou p forem primos
+    if e_primo(k) or e_primo(p):
+        contador += 1
+    return contador
+
+# Definindo um valor máximo fixo para a busca de primos
+MAXIMA_BUSCA_PRIMOS = 10**6
+
+# Realizando a busca de primos até o valor máximo fixo
+primos = []
+for num in range(2, MAXIMA_BUSCA_PRIMOS + 1):
+    if e_primo(num):
+        primos.append(num)
+    
+N = int(input())
+for _ in range(N):
+    k, p = map(int, input().split())
+    quantidade_primos_gemeos = contar_primos_gemeos(primos, k, p)
+    print(quantidade_primos_gemeos)
